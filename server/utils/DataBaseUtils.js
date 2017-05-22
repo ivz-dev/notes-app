f1699595887432d345f0ea248cde49a9b11652b6
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-05-23 01:30:38 +0300

    DataBaseUtils

--- server/utils/DataBaseUtils.js
+++ server/utils/DataBaseUtils.js
@@ -7,3 +7,22 @@ const Note = mongoose.model('Note');
 export function setUpConnection() {
     mongoose.connect('mongodb://localhost/notes');
 }
+
+export function listNotes() {
+    return Node.find();
+}
+
+export function createNote(data) {
+    const note = new Note({
+        title    : data.title,
+        text     : data.text,
+        color    : data.color,
+        createdAt: new Date()
+    });
+
+    return note.save();
+}
+
+export function deleteNote(id){
+    return Note.findById(id).remove();
+}
